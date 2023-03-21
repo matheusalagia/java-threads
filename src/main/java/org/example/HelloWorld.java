9c2439625b7e5ca6b313abdad8fccf9fd88617fa
Matheus Alagia <matheus@ubots.com.br>
2023-03-21 18:42:26 -0300

    Criando exemplo 1 melhorado

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,19 +3,18 @@ package org.example;
 public class HelloWorld {
     public static void main(String[] args) throws InterruptedException {
 
-        // Voce precisa criar um objeto que extende a classe Thread
-        Thread myFirstThread = new Thread() {
-            // sobreescrever o metodo run() com o codigo que voce deseja executar
-            // esse metodo executara concorrentemente ao codigo da thread
-            // principal (main)
-            @Override
-            public void run() {
-                System.out.println("Hello world from my first thread in java!");
-            }
-        };
-        myFirstThread.start();
+        /* equivalente a:
+          Runnable runnable = new Runnable() {
+                      @Override
+                      public void run() {
+                          System.out.println("Hello world using runnable!");
+                      }
+                  };
+         */
+        Runnable runnable = () -> System.out.println("Hello world using runnable!");
+        Thread t = new Thread(runnable);
+        t.start();
         System.out.println("Hello world from main thread!");
-        // aguarda o codigo da thread terminar, ou seja, o codigo do metodo run().
-        myFirstThread.join();
+        t.join();
     }
 }
