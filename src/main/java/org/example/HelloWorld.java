528b2204923046932015fe762174add32427fd09
Matheus Alagia <matheus@ubots.com.br>
2023-03-21 21:57:43 -0300

    Exemplo com runnable

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,16 +2,12 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) throws InterruptedException {
-
-        /* equivalente a:
-          Runnable runnable = new Runnable() {
-                      @Override
-                      public void run() {
-                          System.out.println("Hello world using runnable!");
-                      }
-                  };
-         */
-        Runnable runnable = () -> System.out.println("Hello world using runnable!");
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello world using runnable!");
+            }
+        };
         Thread t = new Thread(runnable);
         t.start();
         System.out.println("Hello world from main thread!");
